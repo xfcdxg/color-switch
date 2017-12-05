f62be89ef2e6bc813f607e9b7a4bb25051b1e677
xfcdxg <xfcdxg_zq@163.com>
2017-12-05 11:29:31 +0800

    next

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/prd/Documents/_git/_personal/color-switch/pages/index.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    }
+  }, "hello");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7OztrQkFDRSxZQUFBO3lCQUFPLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxHQUFBLEVBQVAsQUFBTztBQURUIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9wcmQvRG9jdW1lbnRzL19naXQvX3BlcnNvbmFsL2NvbG9yLXN3aXRjaCJ9
---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default (
+  () => (<div>hello</div>)
+)
