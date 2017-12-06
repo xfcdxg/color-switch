166d23c3a1cb87b566470def78a4720970d40a3f
xfcdxg <xfcdxg_zq@163.com>
2017-12-06 13:02:03 +0800

    reset

---
+++ _next/7401456d-ce17-4a3a-8aa3-a8824de0d2b9/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([5],{219:function(e,t,l){e.exports=l(220)},220:function(e,t,l){"use strict";function o(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=l(1),r=o(u),a=l(117),c=o(a),n=l(85),f=o(n);t.default=function(){return r.default.createElement(c.default,null,r.default.createElement("h1",{style:{color:f.default[0]}},"Im Color Switch"),r.default.createElement("h2",{style:{color:f.default[1]}},"Im Color Switch"),r.default.createElement("h3",{style:{color:f.default[2]}},"Im Color Switch"))}}},[219]);
+            return { page: comp.default }
+          })
+        
